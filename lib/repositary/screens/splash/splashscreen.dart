0f610de0240a7667404695cefc9461bbf4747ef5
nithish25kumar <nithish25may2005@gmail.com>
2025-07-13 10:42:01 +0530

    splash page created

---
+++ lib/repositary/screens/splash/splashscreen.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:zippyit/repositary/screens/bottomnav/bottomnavscreen.dart';
+import 'package:zippyit/repositary/screens/welcomescreen.dart';
+
+//stateful widget becoz it can navigate to another screen in few sec
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  //initialization where setting page duration to 3 seconds
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      //if user has logged in he can navigate to the homescreen
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => Bottomnavscreen()),
+        );
+      } //else will navigate to the welcome screen to login with google
+      else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const Welcomescreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //bg color
+      backgroundColor: Colors.red,
+      //aligning to center
+      body: Center(
+        //child->column->children
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Uihelper.CustomImage(img: "food_delivery.png"),
+            SizedBox(
+              height: 5,
+            ),
+            Uihelper.CustomText(
+              text: "ZippyIt!",
+              color: Colors.black,
+              fontweight: FontWeight.bold,
+              fontsize: 40,
+              fontfamily: "bold",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
